5b51ea6b04bfead8c857444a72be174bfab82654
Katczajk <43179276+Katczajk@users.noreply.github.com>
2020-12-15 11:49:02 +0100

    Update Program.cs
    suggestion to prevent crash, powerful and easy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -25,8 +25,15 @@ namespace emulator_helper
 
                 if (runningProcess.Length == 0)
                 {
-                    Console.WriteLine($"[{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")}] - Emulator started.");
-                    Process.Start("C:\\Users\\Administrator\\Desktop\\Emulator\\darkorbit.exe");
+                    /* try-catch prevent stack error */
+                    try
+                    {
+                        run_server();
+                    } catch
+                    {
+                        Console.WriteLine(error_msg(100));
+                    }
+
                     //Process.Start("C:\\Users\\Yusuf\\Desktop\\darkorbit-emulators\\DarkOrbit 10.0\\bin\\Debug\\darkorbit.exe");
                 }
 
@@ -36,6 +43,25 @@ namespace emulator_helper
                     new WebClient().DownloadString("http://infinityorbit.com/cronjobs/ranking");
                     cronjobTime = DateTime.Now;
                 }
+            }
+        }
+        
+        static void run_server()
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")}] - Emulator started.");
+            Process.Start("C:\\dark\\emu\\DarkOrbit 10.0\\bin\\Debug\\dark_orbit.exe");
+        }
+        
+        static string error_msg(int error_code)
+        {
+            switch (error_code) {
+
+                case 100:
+                    return "Darkorbit.exe not found - Please update Path in Sourcecode!";
+                    break;
+                default:
+                    return "0";
+                    break;
             }
         }
     }
